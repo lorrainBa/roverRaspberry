dcae63141fe72ffb1f508722a21729490fc44c9f
Lorrain <lorrain.elekeval@gmail.com>
2023-06-05 16:11:56 +0100

    init

---
+++ code/test.py
@@ -0,0 +1,235 @@
+import cv2
+import os 
+import numpy as np
+import logging
+import math
+
+import picar
+#Get the image
+frame = cv2.imread('../data/ligneBleu.png')
+height, width, _ = frame.shape
+def detect_edges(frame):
+    # filter for blue lane lines
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    lower_blue = np.array([60, 40, 40])
+    upper_blue = np.array([150, 255, 255])
+    mask = cv2.inRange(hsv, lower_blue, upper_blue)
+   
+    # detect edges
+    edges = cv2.Canny(mask, 200, 400)
+
+    return edges
+
+def region_of_interest(edges):
+    height, width = edges.shape
+    mask = np.zeros_like(edges)
+    # only focus bottom half of the screen
+    polygon = np.array([[
+        (0, height * 1 / 2),
+        (width, height * 1 / 2),
+        (width, height),
+        (0, height),
+    ]], np.int32)
+
+    cv2.fillPoly(mask, polygon, 255)
+    cropped_edges = cv2.bitwise_and(edges, mask)
+    
+    return cropped_edges
+
+def detect_line_segments(cropped_edges):
+    # tuning min_threshold, minLineLength, maxLineGap is a trial and error process by hand
+    rho = 1  # distance precision in pixel, i.e. 1 pixel
+    angle = np.pi / 180  # angular precision in radian, i.e. 1 degree
+    min_threshold = 10  # minimal of votes
+    line_segments = cv2.HoughLinesP(cropped_edges, rho, angle, min_threshold, 
+                                    np.array([]), minLineLength=8, maxLineGap=4)
+    
+
+    return line_segments
+
+def average_slope_intercept(frame, line_segments):
+    """
+    This function combines line segments into one or two lane lines
+    If all line slopes are < 0: then we only have detected left lane
+    If all line slopes are > 0: then we only have detected right lane
+    """
+    lane_lines = []
+    if line_segments is None:
+        logging.info('No line_segment segments detected')
+        return lane_lines
+
+    height, width, _ = frame.shape
+    left_fit = []
+    right_fit = []
+
+    boundary = 1/3
+    left_region_boundary = width * (1 - boundary)  # left lane line segment should be on left 2/3 of the screen
+    right_region_boundary = width * boundary # right lane line segment should be on right 2/3 of the screen
+
+    for line_segment in line_segments:
+        for x1, y1, x2, y2 in line_segment:
+            if x1 == x2:
+                logging.info('skipping vertical line segment (slope=inf): %s' % line_segment)
+                continue
+            fit = np.polyfit((x1, x2), (y1, y2), 1)
+            slope = fit[0]
+            intercept = fit[1]
+            if slope < 0:
+                if x1 < left_region_boundary and x2 < left_region_boundary:
+                    left_fit.append((slope, intercept))
+            else:
+                if x1 > right_region_boundary and x2 > right_region_boundary:
+                    right_fit.append((slope, intercept))
+
+    left_fit_average = np.average(left_fit, axis=0)
+    if len(left_fit) > 0:
+        lane_lines.append(make_points(frame, left_fit_average))
+
+    right_fit_average = np.average(right_fit, axis=0)
+    if len(right_fit) > 0:
+        lane_lines.append(make_points(frame, right_fit_average))
+
+    logging.debug('lane lines: %s' % lane_lines)  # [[[316, 720, 484, 432]], [[1009, 720, 718, 432]]]
+
+    return lane_lines
+
+def make_points(frame, line):
+    height, width, _ = frame.shape
+    slope, intercept = line
+    y1 = height  # bottom of the frame
+    y2 = int(y1 * 1 / 2)  # make points from middle of the frame down
+
+    # bound the coordinates within the frame
+    x1 = max(-width, min(2 * width, int((y1 - intercept) / slope)))
+    x2 = max(-width, min(2 * width, int((y2 - intercept) / slope)))
+    return [[x1, y1, x2, y2]]
+
+def detect_lane(frame):
+    
+    edges = detect_edges(frame)
+    cropped_edges = region_of_interest(edges)
+    line_segments = detect_line_segments(cropped_edges)
+    lane_lines = average_slope_intercept(frame, line_segments)
+    
+    return lane_lines
+
+def display_lines(frame, lines, line_color=(0, 255, 0), line_width=14):
+    line_image = np.zeros_like(frame)
+    if lines is not None:
+        for line in lines:
+            for x1, y1, x2, y2 in line:
+                cv2.line(line_image, (x1, y1), (x2, y2), line_color, line_width)
+    line_image = cv2.addWeighted(frame, 0.8, line_image, 1, 1)
+    return line_image
+
+lane_lines = detect_lane(frame)
+lane_lines_image = display_lines(frame, lane_lines)
+cv2.imshow("lane lines", lane_lines_image)
+cv2.waitKey(0)
+
+
+def display_heading_line(frame, steering_angle, line_color=(0, 0, 255), line_width=5 ):
+    heading_image = np.zeros_like(frame)
+    height, width, _ = frame.shape
+
+    # figure out the heading line from steering angle
+    # heading line (x1,y1) is always center bottom of the screen
+    # (x2, y2) requires a bit of trigonometry
+
+    # Note: the steering angle of:
+    # 0-89 degree: turn left
+    # 90 degree: going straight
+    # 91-180 degree: turn right 
+    steering_angle_radian = steering_angle / 180.0 * math.pi
+    x1 = int(width / 2)
+    y1 = height
+    x2 = int(x1 - height / 2 / math.tan(steering_angle_radian))
+    y2 = int(height / 2)
+
+    cv2.line(heading_image, (x1, y1), (x2, y2), line_color, line_width)
+    heading_image = cv2.addWeighted(frame, 0.8, heading_image, 1, 1)
+
+    return heading_image
+
+def stabilize_steering_angle(
+          curr_steering_angle, 
+          new_steering_angle, 
+          num_of_lane_lines, 
+          max_angle_deviation_two_lines=5, 
+          max_angle_deviation_one_lane=1):
+    """
+    Using last steering angle to stabilize the steering angle
+    if new angle is too different from current angle, 
+    only turn by max_angle_deviation degrees
+    """
+    if num_of_lane_lines == 2 :
+        # if both lane lines detected, then we can deviate more
+        max_angle_deviation = max_angle_deviation_two_lines
+    else :
+        # if only one lane detected, don't deviate too much
+        max_angle_deviation = max_angle_deviation_one_lane
+    
+    angle_deviation = new_steering_angle - curr_steering_angle
+    if abs(angle_deviation) > max_angle_deviation:
+        stabilized_steering_angle = int(curr_steering_angle
+            + max_angle_deviation * angle_deviation / abs(angle_deviation))
+    else:
+        stabilized_steering_angle = new_steering_angle
+    return stabilized_steering_angle
+
+class DeepPiCar(object):
+
+    __INITIAL_SPEED = 0
+    __SCREEN_WIDTH = 320
+    __SCREEN_HEIGHT = 240
+
+    def __init__(self):
+        """ Init camera and wheels"""
+        logging.info('Creating a DeepPiCar...')
+
+        picar.setup()
+
+        logging.debug('Set up camera')
+        self.camera = cv2.VideoCapture(-1)
+        self.camera.set(3, self.__SCREEN_WIDTH)
+        self.camera.set(4, self.__SCREEN_HEIGHT)
+
+        self.pan_servo = picar.Servo.Servo(1)
+        self.pan_servo.offset = -30  # calibrate servo to center
+        self.pan_servo.write(90)
+
+        self.tilt_servo = picar.Servo.Servo(2)
+        self.tilt_servo.offset = 20  # calibrate servo to center
+        self.tilt_servo.write(90)
+
+        logging.debug('Set up back wheels')
+        self.back_wheels = picar.back_wheels.Back_Wheels()
+        self.back_wheels.speed = 0  # Speed Range is 0 (stop) - 100 (fastest)
+
+        logging.debug('Set up front wheels')
+        self.front_wheels = picar.front_wheels.Front_Wheels()
+        self.front_wheels.turning_offset = 22  # calibrate servo to center
+        self.front_wheels.turn(90)  # Steering Range is 45 (left) - 90 (center) - 135 (right)
+
+
+        logging.debug('deep picar end to end lane follower')
+        logging.debug('________________________________________')
+        logging.debug('________________________________________')
+        self.lane_follower = EndToEndLaneFollower(self)
+
+        self.fourcc = cv2.VideoWriter_fourcc(*'XVID')
+        datestr = datetime.datetime.now().strftime("%y%m%d_%H%M%S")
+        self.video_orig = self.create_video_recorder('./data/tmp/car_video%s.avi' % datestr)
+        self.video_lane = self.create_video_recorder('./data/tmp/car_video_lane%s.avi' % datestr)
+        self.video_objs = self.create_video_recorder('./data/tmp/car_video_objs%s.avi' % datestr)
+
+        logging.info('Created a DeepPiCar')
+
+    def drive(self,speedInput=40):
+            self.back_wheels.speed = speedInput
+            
+def main():
+    with DeepPiCar() as car:
+        car.drive(40)
+
+main()
